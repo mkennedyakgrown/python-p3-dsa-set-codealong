50cb5d0ed436e090ff2521cc1845617fd098e99f
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-03-05 12:52:52 -0600

    Completed Lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,24 @@
 class MySet:
 
-    pass
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary = {}
+
+    def __str__(self):
+        return f"MySet: {self.dictionary.keys()}"
